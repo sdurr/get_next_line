ad8b8f2ea9947979d6830e8b99f3ddaf4084b8c8
sdurr <sdurr@e3r1p12.42.fr>
2014-11-17 11:32:26 +0100

    before decoupe

--- get_next_line.c
+++ get_next_line.c
@@ -6,43 +6,58 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/16 09:22:58 by sdurr             #+#    #+#             */
-/*   Updated: 2014/11/16 11:37:36 by sdurr            ###   ########.fr       */
+/*   Updated: 2014/11/17 11:31:08 by sdurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include "libft.h"
+
 
 int		get_next_line(int const fd, char **line)
 {
 	char buf[BUF_SIZE];
 	int ret_read;
-	int c;
+	static int c;
+	char *stock;
+	int i;
 
+	i = 0;
 	c = 0;
-	if (BUF_SIZE == 0)
+	if (!(stock = (char *)malloc(sizeof(char) * 1000)))
+		return (-1);
+		if (BUF_SIZE == 0)
 		return (-1);
 	while (ret_read = (read(fd, buf, BUF_SIZE)))
 	{
-	     if (buf[ret_read] == '\n' || buf[ret_read] == '\0')
+		stock[i] = buf[c];
+
+		//	ft_putchar('\n');
+		//ft_putnbr(i);
+	if ((stock[i] == '\n') || (stock[i] == '\0'))
 	     {
-		  if (line)
-		       free(line);
-		  if (!((*line)=(char *)malloc(sizeof(char) * ret_read)))
-		       return (-1);
-		  while (c < ret_read)
-		     {
-			  *(*line) = buf[c];
-			  (*line)++;
-			  c++;
-		     }
-		  if (c == ret_read)
-		  {
-		       *(*line) = '\0';
-		       return (1);
-		  }
-		  if (buf[ret_read] == '\0')
-		       return (0);
+			 if (!(line = malloc(sizeof(char) * 1000)))
+				 return (-1);
+			 i = 0;
+			 if (!(*line = (char *)malloc(sizeof(char) * 1000)))
+				 return (-1);
+			 while (stock[i] != '\0' && stock[i] != '\n')
+			 {
+				 // ft_putnbr(i);
+				 (*line)[i] = stock[i];
+				 i++;
+			 }
+			 //		 save = c;
+			 ft_putstr(stock);
+			 if (stock[i] != '\0')
+			 {
+				 (*line)[i] = '\0';
+				 return (1);
+			 }
+			 if (stock[i] == '\0')
+				 return (0);
 	     }
+		i++;
 	}
 	return (-1);	
 }
