72e9f117a3e821cb08250beda9dd2eb7a07daaac
sdurr <sdurr@e3r1p12.42.fr>
2014-11-23 00:57:33 +0100

    test update

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,67 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/16 09:22:58 by sdurr             #+#    #+#             */
-/*   Updated: 2014/11/22 22:35:35 by sdurr            ###   ########.fr       */
+/*   Updated: 2014/11/23 00:33:45 by sdurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "libft.h"
+
+size_t  ft_strlen(const char *s)
+{
+	size_t cpt;
+
+	cpt = 0;
+	while (*s != '\0')
+	{
+		cpt++;
+		s++;
+	}
+	return (cpt);
+}
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+ 	int c;
+	char *ret;
+	int i;
+	
+	i = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	c = ft_strlen(s1) + ft_strlen(s2);
+	if (!(ret = (char *)malloc(sizeof(char*) * c)))
+		return (NULL);
+	while (*s1 != '\0')
+	{
+		ret[i++] = *s1;
+		s1++;
+	}
+	while (*s2 != '\0')
+	{
+		ret[i++] = *s2;
+		s2++;
+	}
+	ret[i] = '\0';
+	return (ret);
+}
+
+char  *ft_strnew(size_t size)
+{
+	char *ret;
+	int i;
+
+	i = 0;
+	if (!(ret = (char *)malloc(sizeof(char*) * size)))
+		return (NULL);
+	while (size != 0)
+	{
+		ret[i] = '\0';
+		size--;
+		i++;
+	}
+	return (ret);
+}
 
 int		ft_line(char *s, int i)
 {
@@ -35,6 +90,8 @@ int		get_next_line(int const fd, char **line)
 	static char		*stock;
 	static	int		j = 0;
 
+	if(line == NULL)
+		return (-1);
 	if (!stock)
 		stock = ft_strnew(1);
 	while ((i = read(fd, buf, BUFF_SIZE)) != EOF && i > 0)
