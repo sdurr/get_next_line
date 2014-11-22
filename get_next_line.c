a60ea70e14d26f3ff2b238e5161ee80a9d1adafa
sdurr <sdurr@e3r1p12.42.fr>
2014-11-22 14:31:56 +0100

    to update

--- get_next_line.c
+++ get_next_line.c
@@ -6,21 +6,21 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/16 09:22:58 by sdurr             #+#    #+#             */
-/*   Updated: 2014/11/22 00:48:53 by sdurr            ###   ########.fr       */
+/*   Updated: 2014/11/22 14:28:49 by sdurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft.h"
 
-int		ft_line_malloc(char *s, char c, int i)
+int		ft_line(char *s, int i)
 {
 	int len;
 	int j;
 
 	j = i;
 	len = 1;
-	while (s[j] != c && s[j] != '\0')
+	while (s[j] != '\n' && s[j] != '\0')
 	{
 		len++;
 		j++;
@@ -30,71 +30,29 @@ int		ft_line_malloc(char *s, char c, int i)
 
 int		get_next_line(int const fd, char **line)
 {
-	char  buf[BUFF_SIZE];
-	char *stock;
-	int i;
-	static	int j = 0;
-	int ret_read;
+	char			buf[BUFF_SIZE + 1];
+	int				i;
+	static char		*stock;
+	static	int		j = 0;
 
-	i = 0;
-	stock = ft_strnew(1);
-
-	while ((ret_read = read(fd, buf, BUFF_SIZE)) == 1)
+	if (!stock)
+		stock = ft_strnew(1);
+	while ((i = read(fd, buf, (BUFF_SIZE - BUFF_SIZE + 1))) != EOF && i == 1)
 	{
 		buf[BUFF_SIZE] = '\0';
 		stock = ft_strjoin(stock, buf);
 	}
-
-	ft_putchar('\n');
-	ft_putnbr(j);
-	ft_putchar('\n');
-	ft_putstr(stock);
-	ft_putchar('\n');
-
+	i = 0;
+	if (stock[j] == '\n')
+		j++;
 	while (stock[j] != '\0')
 	{
-		if(!(line = (char**)malloc(sizeof(char) * 1)))
-		{
-			ft_putstr("error malloc 1");
+		if (!(*line = (char*)malloc(sizeof(char) * ft_line(stock, j) + 1)))
 			return (-1);
-		}
-		if(!(*line = (char*)malloc(sizeof(char) * ft_line_malloc(stock, '\n', j) + 1)))
-		{
-			ft_putstr("error malloc 2");
-			return (-1);
-		}
-		while (stock[j] != '\n')
-		{
-			ft_putnbr(i);
-			ft_putchar(' ');
-			ft_putnbr(j);
-			ft_putchar('\n');
-			(*line)[i] = stock[j];
-			ft_putchar(stock[j]);
-			ft_putchar('\n');
-			//ft_putnbr(i);
-			//ft_putstr(*line);
-			//ft_putchar('\n');
-			//ft_putchar(stock[j]);
-			//ft_putchar('\n');		
-			j++;
-			i++;
-		}
-		j++;
-		//ft_putnbr(j);
+		while (stock[j] != '\n' && stock[j] != '\0')
+			(*line)[i++] = stock[j++];
 		(*line)[i] = '\0';
-		ft_putstr("line = ");
-		ft_putstr(*line);
-		ft_putchar('\n');
-		if (stock[j] != '\0')
-			return (1);
-		else
-			return (0);
-		//ft_putchar('\n');
-		//ft_putstr(stock);
+		return (1);
 	}
-	//ft_putchar('\n');
-	//ft_putnbr(j);
-	ft_putstr("error");
-	return (-1);
+	return (0);
 }
