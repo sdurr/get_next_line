10e6a96caf0ec6c08c8c03dd66cdcfb8d9f51079
sdurr <sdurr@e3r1p10.42.fr>
2014-11-28 21:59:06 +0100

    ok fd 0 but sgv eof ..

--- get_next_line.c
+++ get_next_line.c
@@ -6,63 +6,45 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/16 09:22:58 by sdurr             #+#    #+#             */
-/*   Updated: 2014/11/24 14:06:13 by sdurr            ###   ########.fr       */
+/*   Updated: 2014/11/28 21:54:14 by sdurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft.h"
+#include <stdlib.h>
 
-int		ft_line(char *s, int i)
+char	*ft_read(char *stock, int const fd, int *i)
 {
-	int		len;
-	int		j;
+	char			buf[BUFF_SIZE + 1];
 
-	j = i;
-	len = 1;
-	while (s[j] != '\n' && s[j] != '\0')
-	{
-		len++;
-		j++;
-	}
-	return (len);
-}
-
-void	ft_read(char **stock, char *buf, int const fd)
-{
-	int i;
-
-	while ((i = read(fd, buf, BUFF_SIZE)) != EOF && i > 0)
-	{
-		buf[i] = '\0';
-		*stock = ft_strjoin(*stock, buf);
-	}
+	if ((*i = read(fd, buf, BUFF_SIZE)) != EOF)
+		buf[*i] = '\0';
+	stock = ft_strjoin(stock, buf);
+	return (stock);
 }
 
 int		get_next_line(int const fd, char **line)
 {
-	char			buf[BUFF_SIZE + 1];
 	int				i;
 	static char		*stock;
-	static	int		j = 0;
+	char			*stock2;
 
-	if (line == NULL)
+	if (line == NULL || fd < 0)
 		return (-1);
 	if (!stock)
 		stock = ft_strnew(1);
-	ft_read(&stock, buf, fd);
-	i = 0;
-	if (stock[j] == '\n')
-		j++;
-	while (stock[j] != '\0')
+	i = 1;
+	while (i > 0)
 	{
-		if (!(*line = (char*)malloc(sizeof(char) * ft_line(stock, j) + 1)))
-			return (-1);
-		while (stock[j] != '\n' && stock[j] != '\0')
-			(*line)[i++] = stock[j++];
-		(*line)[i] = '\0';
-		return (1);
+		if ((stock2 = ft_strchr(stock, '\n')) != NULL)
+		{
+			*stock2 = '\0';
+			*line = ft_strdup(stock);
+			stock = ft_strnew(1);
+			return (1);
+		}
+		stock = ft_read(stock, fd, &i);
 	}
-	(*line)[i] = '\0';
-	return (0);
+	return (i);
 }
