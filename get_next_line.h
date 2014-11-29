8931b154841426778c3a117c938915fa32920022
sdurr <sdurr@e3r1p9.42.fr>
2014-11-29 18:42:28 +0100

    .h

--- get_next_line.h
+++ get_next_line.h
@@ -6,22 +6,14 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/16 09:23:11 by sdurr             #+#    #+#             */
-/*   Updated: 2014/11/23 00:38:31 by sdurr            ###   ########.fr       */
+/*   Updated: 2014/11/29 18:38:19 by sdurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
-# include <stdio.h>
-# include <string.h>
-# include <stdlib.h>
-# include <fcntl.h>
-# include <sys/types.h>
-# include <sys/stat.h>
-
-# define BUFF_SIZE 1000000
+# define BUFF_SIZE 100
 
 int		get_next_line(int const fd, char **line);
 #endif
