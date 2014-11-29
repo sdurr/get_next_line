1c29799b48a6a941e2e9a247b74ff2de79b80007
sdurr <sdurr@e3r1p9.42.fr>
2014-11-29 18:42:09 +0100

    gnl ok

--- get_next_line.c
+++ get_next_line.c
@@ -6,28 +6,33 @@
 /*   By: sdurr <sdurr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/16 09:22:58 by sdurr             #+#    #+#             */
-/*   Updated: 2014/11/28 21:54:14 by sdurr            ###   ########.fr       */
+/*   Updated: 2014/11/29 18:41:38 by sdurr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft.h"
 #include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdio.h>
 
 char	*ft_read(char *stock, int const fd, int *i)
 {
-	char			buf[BUFF_SIZE + 1];
+	char			buf[BUFF_SIZE];
 
-	if ((*i = read(fd, buf, BUFF_SIZE)) != EOF)
+	if ((*i = read(fd, buf, BUFF_SIZE)) != EOF && *i > 0)
+	{
 		buf[*i] = '\0';
-	stock = ft_strjoin(stock, buf);
+		stock = ft_strjoin(stock, buf);
+	}
 	return (stock);
 }
 
 int		get_next_line(int const fd, char **line)
 {
 	int				i;
-	static char		*stock;
+	static char		*stock = NULL;
 	char			*stock2;
 
 	if (line == NULL || fd < 0)
@@ -37,14 +42,15 @@ int		get_next_line(int const fd, char **line)
 	i = 1;
 	while (i > 0)
 	{
+		stock = ft_read(stock, fd, &i);
 		if ((stock2 = ft_strchr(stock, '\n')) != NULL)
 		{
 			*stock2 = '\0';
 			*line = ft_strdup(stock);
-			stock = ft_strnew(1);
+			ft_memmove(stock, stock2 + 1, ft_strlen(stock2 + 1) + 1);
 			return (1);
 		}
-		stock = ft_read(stock, fd, &i);
 	}
-	return (i);
+	*line = ft_strdup(stock);
+	return (0);
 }
